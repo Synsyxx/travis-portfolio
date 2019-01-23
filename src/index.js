752dc72a7c54eea2cadcab8fe92f7a984895904f
Travis Simpson <travis.simpson92@hotmail.com>
2019-01-22 22:58:08 -0500

    started working on projectCards layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Card from './components/contactcard/contactCard';
+import projectCards from './components/projectcards/projectCards';
 
 class App extends Component {
     render() {
